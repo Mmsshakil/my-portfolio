9a593eb8532725c52f5dd91adaf48eb282735ab8
Mmsshakil <softhandshakil@gmail.com>
2023-12-12 18:53:08 +0600

    contact page done

--- src/home/Contact/Contact.jsx
+++ src/home/Contact/Contact.jsx
@@ -1,34 +1,76 @@
 
+import { MdEmail, MdPhoneEnabled, MdLocationOn } from "react-icons/md";
+import { FaLinkedin, FaGithub } from "react-icons/fa6";
+
 const Contact = () => {
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-2 justify-center items-center gap-8 md:gap-16  py-10 w-full">
-            <div className="text-center ">
-                <h1 className="text-5xl font-bold mb-4">Contact with Us</h1>
-                <p className="text-xl font-bold mb-2">Blood Bank Comunity.</p>
-                <p className="">12 / 22 Salimullah Road,<br /> Mohammadpur, Dhaka 1207</p>
-                <div className="mt-2">
-                    <p>Email: softhandshakil@gmail.com</p>
-                    <p>Phone: +8809638910651</p>
-                </div>
-            </div>
-            <div className="w-full">
-                <form className="">
-                    <label className="label">
-                        <span className="label-text">Enter Your Name</span>
-                    </label>
-                    <input className="p-3 rounded-md w-3/4 bg-slate-900" type="text" placeholder="Name" />
-                    <label className="label mt-2">
-                        <span className="label-text">Enter Your Email</span>
-                    </label>
-                    <input className="p-3 rounded-md w-3/4 bg-slate-900" type="text" placeholder="Email" />
-                    <label className="label mt-2">
-                        <span className="label-text">Enter Your Comment</span>
-                    </label>
-                    <textarea className="textarea textarea-bordered w-3/4 bg-slate-900" placeholder="Comment..."></textarea>
-                    <div className="mt-3">
-                        <button className="btn btn-outline btn-accent">Submit</button>
+        <div>
+            <div className="divider divider-success text-4xl font-bold text-white w-1/2 mx-auto mt-14">Contact Me</div>
+
+            <div className="grid grid-cols-1 lg:grid-cols-2 justify-center items-center gap-8 md:gap-16  py-10 w-full">
+                <div className="text-center flex flex-col justify-center items-center">
+                    <h1 className=" text-lg md:text-xl lg:text-3xl font-bold text-white">Contact Info</h1>
+
+
+                    <div className="mt-7 space-y-2 md:space-y-4">
+
+                        <div className="flex justify-center items-center">
+                            <MdEmail className="text-2xl font-bold mr-2 text-blue-600"></MdEmail>
+                            <p className="text-xl"><span className="font-bold text-white">Email :</span> softhandshakil@gmail.com</p>
+                        </div>
+
+
+
+                        <div className="flex gap-5 justify-center items-center">
+                            <div className="flex justify-center items-center">
+                                <FaLinkedin className="text-2xl font-bold mr-2 text-blue-600"></FaLinkedin>
+                                <p className="text-xl"><a className="font-bold text-white" href="https://www.linkedin.com/in/mmsshakil">Linkedin</a></p>
+                            </div>
+
+                            <div className="flex justify-center items-center">
+                                <FaGithub className="text-2xl font-bold mr-2 text-blue-600"></FaGithub>
+                                <p className="text-xl"><a className="font-bold text-white" href="https://github.com/Mmsshakil">GitHub</a></p>
+                            </div>
+                        </div>
+
+                        <div className="flex justify-center items-center">
+                            <MdPhoneEnabled className="text-2xl font-bold mr-2 text-blue-600"></MdPhoneEnabled>
+                            <p className="text-xl"><span className="font-bold text-white">Phone :</span> +8801786829816</p>
+                        </div>
+                        <div className="flex justify-center items-center">
+                            <MdLocationOn className="text-2xl font-bold mr-2 text-blue-600"></MdLocationOn>
+                            <p className="text-xl"><span className="font-bold text-white">Address :</span>  Mohammadpur | Dhaka</p>
+                        </div>
+
                     </div>
-                </form>
+                </div>
+                <div className="w-full ">
+                    <form className=" w-full mx-auto flex flex-col px-5 lg:px-0">
+                        <div>
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input className="p-3 rounded-md w-full lg:w-3/4 bg-slate-900" type="text" placeholder="Name" />
+                        </div>
+
+                        <div>
+                            <label className="label mt-2">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input className="p-3 rounded-md w-full lg:w-3/4 bg-slate-900" type="text" placeholder="Email" />
+                        </div>
+
+                        <div>
+                            <label className="label mt-2">
+                                <span className="label-text">Message</span>
+                            </label>
+                            <textarea className="textarea textarea-bordered w-full lg:w-3/4 bg-slate-900" placeholder="Message..."></textarea>
+                        </div>
+                        <div className="mt-3">
+                            <button className="btn btn-info btn-outline">Submit</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     );
